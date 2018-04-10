67c0a235935cc43374c903a318e74366154cbaab
Robin Sola <rsolarms@gmail.com>
2018-04-09 17:22:38 -0700

    add constructor, prototype, and styling.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
+var type = "";
 
-// function Triangle(side1, side2, side3) {
-//   this.firstSide = side1;
-//   this.secondSide = side2;
-//   this.thirdSide = side3;
-// }
-
-var sideSum1 = function(side1, side2) {
-  return side1 + side2;
+function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
 };
 
-var sideSum2 = function(side2, side3) {
-  return side2 + side3;
+Triangle.prototype.checker = function(side1, side2, side3) {
+  if (isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) {
+    type = "I can't compute that";
+  } else if ((this.side1 + this.side2 <= this.side3) || (this.side2 + this.side3 <= this.side1) || (this.side3 + this.side1 <= this.side2)) {
+    type = "not a triangle";
+  } else if (this.side1 === this.side2 && this.side1 === this.side3) {
+  type = "equilateral";
+  } else if ((this.side1 === this.side2 && this.side2 !== this.side3) || (this.side2 === this.side3 && this.side3 !== this.side1) || (this.side3 === this.side2 && this.side2 !== this.side1)) {
+    type = "isosceles";
+  } else if (this.side1 !== this.side2 && this.side2 !== this.side3) {
+    type = "scalene";
+  }
+  return type;
 };
-
-var sideSum3 = function(side1, side3) {
-  return side1 + side3;
-};
-
 
 $(document).ready(function() {
-  $("form#triangle-tracker").submit(function(event) {
-   var Side1 = parseInt($("input#side1").val());
-   var Side2 = parseInt($("input#side2").val());
-   var Side3 = parseInt($("input#side3").val());
-
-  //  var newTriangle = new Triangle(inputtedSide1, inputtedSide2, inputtedSide3);
-
-   if(sideSum1(side1,side2) <= side3 || sideSum2(side2,side3) <= side1 || sideSum3(side1,side3) <= side2) {
-      $('#not-triangle').show();
-    } else if (side1 === side2 && side1 === side3) {
-     $('#equilateral').show();
-    } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-      $('#isosceles').show();
-    } else if (side1 != side2 && side1 != side3 && side2 != side3) {
-      $('#scalene').show();
-    }
-
-   event.preventDefault();
+  $("#sides").submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+    var newTriangle = new Triangle(side1, side2, side3);
+    var result = newTriangle.checker();
+    $("#result").text(result);
   });
 });
